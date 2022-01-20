b375b5d7479323f900cbaa1571a25c4d178eb546
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-20 16:09:40 +0100

    start exercise 2 but not finish first

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ console.log('JS OK!')
 /*
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
 */
 
 // BONUS: Stampare in pagina oltre che in console!
 
-const bike = [
+/* const bike = [
     {
         name: 'Bici da città',
         weight: 30,
@@ -36,12 +36,10 @@ const bike = [
     },
 ];
 
-const info = [...bike];
-
-for (let key in info) {
-    const {weight} = info[key];
-    console.log(weight)
-}
+for (let key in bike) {
+    const {name, weight} = bike[key];
+    console.log(`La bici che pesa di meno è ${name} ${weight}`)
+} */
 
 // Snack 2
 /* 
@@ -54,3 +52,47 @@ Infine usando la destrutturazione creiamo un nuovo array,
 */
 
 // BONUS: Stampare in pagina oltre che in console!
+
+// # Function
+
+const randomNumber = (min) => {
+    const number = Math.floor(Math.random() * (min + 1));
+    console.log(number);
+    return number;
+}
+
+const team = [
+    {
+        name: 'Inter',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Milan',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Napoli',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Atalanta',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Juventus',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Lazio',
+        points: 0,
+        fouls: 0,
+    },
+];
+
+const number = randomNumber(20);
+
