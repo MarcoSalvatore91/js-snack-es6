30c3c19d8691bd24173f14234912baf16ec1292f
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-21 15:53:15 +0100

    start snack 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log('JS OK!')
 
-// Snack 1
+// # Snack 1
 /*
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
@@ -50,7 +50,7 @@ for (let item in bikes) {
 
 console.log(`La bici che pesa di meno è la ${minWeight.name}`); */
 
-// Snack 2
+// # Snack 2
 /* 
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
@@ -62,12 +62,14 @@ Infine usando la destrutturazione creiamo un nuovo array,
 
 // BONUS: Stampare in pagina oltre che in console!
 
-// # Function
+// ! Function
 
-const randomNumber = (min) => {
+/* const randomNumber = (min) => {
     const number = Math.floor(Math.random() * (min + 1));
     return number;
 }
+
+const display = document.getElementById('display');
 
 const teams = [
     {
@@ -110,14 +112,66 @@ for (let i = 0; i < teams.length; i++) {
     
     teams[i].points += randomNumber(100);
     teams[i].fouls += randomNumber(100);
+    
     fouls = teams[i].fouls;
-
     result.push({name, fouls})
     
 }
 
-console.log(result);
+console.log(result); */
+
+// # SNACK 3
+
+/*
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array visti oggi in classe.
+*/
+
+/* const number = (arr, a, b) => {
+    const arr = [a, b];
+    if (item <= b) {
+    const number = arr.map((item) => {
+            return (a + 1);
+        });
+    }
+    console.log(arr);
+}
+
+let randomNumber;
+
+const betweenNumbers = number(randomNumber, 2, 10); */
 
 
+// # SNACK 4
 
-/* console.log(teams); */
+/*
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, nome e somma totale dei loro voti di esame...
+1. dobbiamo creare una lista contenente delle targhe (stringhe) con il loro nome in maiuscolo
+ES (marco de iulio => Marco de iulio);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+*/
+
+// ! BONUS  nei nomi sulle targhe l'iniziale di ogni parola deve essere maiuscola
+// ! ES.: (marco de iulio  => Marco De Iulio)
+
+const students = [
+    { name: 'robert de niro', id: 197, grades: 32 },
+    { name: 'cristiano ronaldo', id: 111, grades: 67 },
+    { name: 'sam fisher', id: 36, grades: 12 },
+    { name: 'matt damon', id: 83, grades: 44 },
+    { name: 'harry potter', id: 51, grades: 54 },
+    { name: 'christian de sica', id: 312, grades: 5 },
+];
+
+const allName = [];
+
+students.forEach((student) => {
+    const firstLetter = student.name.charAt().toUpperCase();
+    const otherLetters = student.name.substring(1).toLowerCase();
+    const name = firstLetter + otherLetters;
+    allName.push({name});
+})
+
+console.log(allName);
